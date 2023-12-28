12e446402e2f37628672df91aa7208621efa323b
Leanh <datletb789@gmail.com>
2023-12-28 15:42:27 +0700

    add

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -13,7 +13,7 @@ const Header = (props: Props) => {
                 <div className="flex space-x-4">
                     <img src={Logo} alt="logo" className="w-[6rem] h-[4rem] object-contain" />
                     <div className="pt-4 space-x-4">
-                        <Link to="/" className="text-white hover:bg-slate-100 hover:text-black p-5 rounded-lg">Home</Link>
+                        <Link to="/home" className="text-white hover:bg-slate-100 hover:text-black p-5 rounded-lg">Home</Link>
                         <Link to="/about" className="text-white hover:bg-slate-100 hover:text-black p-5 rounded-lg">About</Link>
                         <Link to="/" className="text-white hover:bg-slate-100 hover:text-black p-5 rounded-lg">Contact</Link>
                     </div>
