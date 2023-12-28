e41e3f90ccf505d6d911168d10120eab479729d9
Leanh <datletb789@gmail.com>
2023-12-28 19:03:44 +0700

    delete feature

--- src/Pages/homePage/Table.tsx
+++ src/Pages/homePage/Table.tsx
@@ -8,6 +8,7 @@ interface Props {
 
 
 const Table = ({ users, setUsers }: Props) => {
+
     return (
         <>
             <div className="p-10">
@@ -44,8 +45,13 @@ const Table = ({ users, setUsers }: Props) => {
                                     <td>{user.phoneNumber}</td>
                                     <td>{user.description}</td>
                                     <td>{user.createdDate}</td>
-                                    <td>
-                                        <Link to={`/crm-user/${user.uuid}`} className="bg-blue-500 text-white font-bold py-2 px-3 rounded">Detail</Link>
+                                    <td className="space-x-4">
+                                        <button className="bg-blue-500 text-white font-bold py-2 px-3 rounded">
+                                            <Link to={`/crm-user/${user.uuid}`}>Detail</Link>
+                                        </button>
+                                        <button className="bg-red-500 text-white font-bold py-2 px-3 rounded">
+                                            <Link to={`/delete/${user.id}`}>Delete</Link>
+                                        </button>
                                     </td>
                                 </tr>
                         ))}
