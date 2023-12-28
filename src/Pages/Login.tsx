51dde93d06e6d5701eb477b57aeab8c8e33e27c8
ngocduc202 <ngocduc1542002@gmail.com>
2023-12-28 15:15:00 +0700

    Create form Login

--- src/Pages/Login.tsx
+++ src/Pages/Login.tsx
@@ -33,16 +33,16 @@ const Login = () => {
 
   return (
     <div className='p-[38px] bg-gradient-to-r from-[#07bd89] to-[#006e8c]'>
-      <div className='relative h-[700px] w-[500px] m-auto shadow-[0px 30px 125px -5px #000] rounded-xl'>
+      <div className='relative h-[700px] w-[500px] m-auto shadow-2xl'>
         <div className='w-full'>
           <img src={LoginImg} alt="LoginImg" className='relative w-full h-[250px] bg-cover' />
           <div className='absolute top-[175px] z-10'>
             <h2 className='pl-[15px] text-[25px] uppercase inline-block font-light text-[#fff] '>Sign In</h2>
-            <h2 className='pl-[15px] text-[25px] uppercase inline-block font-light text-colorRgb '>Sign Up</h2>
+            <h2 className='pl-[15px] text-[25px] uppercase inline-block font-light text-colorRGB'>Sign Up</h2>
           </div>
           <p className='absolute z-50 text-[#07bd89] top-[227px] pl-[50px] text-[20px]'>&#9650;</p>
         </div>
-        <div className='w-full h-[400px] pt-5 bg-gradient-to-r from-[#07bd89] to-[#006e8c]'>
+        <div className='w-full h-[450px] pt-5 bg-gradient-to-r from-[#07bd89] to-[#006e8c] '>
           <form onSubmit={handleSubmit}>
             <div className='flex gap-10 flex-col w-full'>
               <div className='flex flex-col gap-2  items-center '>
