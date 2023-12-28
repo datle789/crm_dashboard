9e31ba141df2399cda40240cdde18ea5b9f744ea
Leanh <datletb789@gmail.com>
2023-12-28 19:09:26 +0700

    main

--- src/Pages/homePage/NavSide.tsx
+++ src/Pages/homePage/NavSide.tsx
@@ -5,7 +5,7 @@
 const NavSide = () => {
     return (
         <>
-            <div className="bg-gradient-to-r from-[#07bd89] to-[#006e8c] min-h-full">
+            <div className="bg-gradient-to-b from-[#07bd89] to-[#006e8c] min-h-full">
                 Avatar
                 <div>image</div>
             </div>
