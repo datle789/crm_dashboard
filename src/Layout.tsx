b0e86378cde33bb6a77e2320939bfef00ff6fac2
Leanh <datletb789@gmail.com>
2024-01-06 18:35:22 +0700

    check token

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,7 +1,9 @@
 import Header from "./components/header/Header"
 import Footer from "./components/footer/Footer"
-import React from "react";
+import React, { useEffect, useState } from "react";
 import NavSide from "./Pages/homePage/NavSide";
+import { useNavigate } from "react-router-dom";
+import { getAccessToken } from "./Pages/type/GetAccessToken";
 
 interface LayoutProps {
     children: React.ReactNode;
@@ -9,13 +11,26 @@ interface LayoutProps {
 
 const Layout: React.FC<LayoutProps> = ({ children }) => {
 
+    const navigate = useNavigate()
+    const [check, setCheck] = useState(false)
+
+    useEffect(() => {
+        const AccessToken = getAccessToken('access_token');
+
+        if (!AccessToken) {
+            navigate('/login')
+        } else {
+            setCheck(true)
+        }
+    }, []);
+
     return (
         <main>
             <Header />
-            <div className="flex">
+            {check ? <div className="flex">
                 <NavSide />
                 <div className="basis-[86%] flex-auto">{children}</div>
-            </div>
+            </div> : ''}
             <Footer />
         </main>
     )
