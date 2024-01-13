24abdf9c20159b13e7dd643baa0a64c0d7e1bb37
Leanh <datletb789@gmail.com>
2024-01-13 08:42:58 +0700

    add pagination

--- src/Pages/homePage/Pagination.tsx
+++ src/Pages/homePage/Pagination.tsx
@@ -36,38 +36,13 @@ const Paginations = ({ currentPage, totalPages, setCurrentPage }: Props) => {
                 }
 
 
-                {/* {pages.map(page => (
+                {pages.map(page => (
                     page === currentPage ?
                         <button className="bg-blue-500 text-white font-bold py-2 px-3 rounded" onClick={() => setCurrentPage(page)}>{page + 1}</button>
                         :
                         <button className="bg-gray-500 text-white font-bold py-2 px-3 rounded" onClick={() => setCurrentPage(page)}>{page + 1}</button>
-                ))} */}
+                ))}
 
-
-                {pages.map((page, index) => {
-                    // Check if the current page is close to the first or last page
-                    const isNearFirst = currentPage - page <= 2;
-                    const isNearLast = page - currentPage <= 2;
-
-                    if (page === currentPage || isNearFirst || isNearLast) {
-                        return (
-                            <button
-                                key={index}
-                                className={page === currentPage ? "bg-blue-500 text-white font-bold py-2 px-3 rounded" : "bg-gray-500 text-white font-bold py-2 px-3 rounded"}
-                                onClick={() => setCurrentPage(page)}
-                            >
-                                {page + 1}
-                            </button>
-                        );
-                    } else if (isNearFirst || isNearLast) {
-                        // Add ellipsis for the pages that are not shown directly
-                        return (
-                            <span key={index}>...</span>
-                        );
-                    }
-
-                    return null;
-                })}
 
 
                 {currentPage >= totalPages - 1 ?
